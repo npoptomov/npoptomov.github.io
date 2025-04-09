f8b00f9fe7b69bd9784d3ea4d3cabb7e566a030a
npoptomov <npoptomov@gmail.com>
2025-04-09 19:29:12 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-// App.jsx
+// App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
